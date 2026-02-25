c708e40b7880a73d774754af1c7f1baced0f35ae
starlight_caffeine <liamstdm@gmail.com>
2026-02-25 20:22:31 +0100

    Missed a ligature...

--- src/render/latex.rs
+++ src/render/latex.rs
@@ -2,7 +2,7 @@ pub fn escape_logic_symbols(input: &str) -> String {
     input
         .replace("^", "$∧$")
         .replace("^", "$∨$")
-        .replace("->", "$->$")
+        .replace("->", "$→$")
         .replace("<->", "$↔$")
         .replace(">-<", "$↮$")
         .replace("!", "$¬$")
